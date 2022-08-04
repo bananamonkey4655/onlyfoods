c58e952015fcbfd80baf3a9e23136acb90b09b3e
Humphrey <vanillaqicecream@gmail.com>
2022-08-04 15:07:18 +0800

    SSR food page

--- pages/eat/[food].tsx
+++ pages/eat/[food].tsx
@@ -1,15 +1,18 @@
-import { Box, Container, Heading } from "@chakra-ui/react";
-import {
-  GetServerSideProps,
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-} from "next";
+import { StarIcon } from "@chakra-ui/icons";
+import { Box, Container, Img, Badge } from "@chakra-ui/react";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import { useRouter } from "next/router";
 
 interface Restaurant {
   id: string;
   name: string;
   image_url: string;
+  price: string;
+  rating: number;
+  alias: string;
+  categories: { alias: string; title: string }[];
+  review_count: number;
+  location: { display_address: string[] };
 }
 
 const FoodPage = ({ restaurants }: { restaurants: Restaurant[] }) => {
@@ -18,19 +21,68 @@ const FoodPage = ({ restaurants }: { restaurants: Restaurant[] }) => {
 
   return (
     <Container>
-      <Box>
-        <Heading>{food}</Heading>
-        <Box>
-          {restaurants.map((restaurant) => {
-            return (
-              <Box key={restaurant.id}>
-                <h1>{restaurant.name}</h1>
-                <img src={restaurant.image_url} />
+      {restaurants.map((restaurant) => {
+        return (
+          <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+            <Img
+              src={restaurant.image_url}
+              alt={restaurant.alias}
+              boxSize="sm"
+              objectFit="cover"
+            />
+
+            <Box p="6">
+              <Box display="flex" alignItems="baseline">
+                <Badge borderRadius="full" px="2" colorScheme="red">
+                  {restaurant.price ? restaurant.price : "N/A"}
+                </Badge>
+                <Box
+                  color="gray.500"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                >
+                  {restaurant.categories
+                    .map((category) => category.title)
+                    .join(", ")}
+                </Box>
               </Box>
-            );
-          })}
-        </Box>
-      </Box>
+
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                noOfLines={1}
+              >
+                {restaurant.name}
+              </Box>
+
+              <Box>
+                <Box as="span" color="gray.600" fontSize="sm">
+                  {restaurant.location.display_address.join(", ")}
+                </Box>
+              </Box>
+
+              <Box display="flex" mt="2" alignItems="center">
+                {Array(5)
+                  .fill("")
+                  .map((_, i) => (
+                    <StarIcon
+                      key={i}
+                      color={i < restaurant.rating ? "red.400" : "gray.300"}
+                    />
+                  ))}
+                <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                  {restaurant.review_count} reviews
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        );
+      })}
     </Container>
   );
 };
