a795f453cbf3e02bb42c48f96144acffe58ab57f
Humphrey <vanillaqicecream@gmail.com>
2022-08-06 17:05:43 +0800

    Add more food images

--- utils/foodData.ts
+++ utils/foodData.ts
@@ -5,6 +5,11 @@ import burger from "../public/burger.png";
 import iceCream from "../public/ice-cream.png";
 import chicken from "../public/chicken.png";
 import pizza from "../public/pizza.png";
+import breakfast from "../public/breakfast.png";
+import cake from "../public/cake.png";
+import donut from "../public/donut.png";
+import spaghetti from "../public/spaghetti.png";
+import waffle from "../public/waffle.png";
 
 export const foodData = [
   { name: "pancake", image: pancake },
@@ -14,4 +19,9 @@ export const foodData = [
   { name: "ice_cream", image: iceCream },
   { name: "chicken", image: chicken },
   { name: "pizza", image: pizza },
+  { name: "breakfast", image: breakfast },
+  { name: "cake", image: cake },
+  { name: "donut", image: donut },
+  { name: "spaghetti", image: spaghetti },
+  { name: "waffle", image: waffle },
 ];
