cc76f44a403acc5de904201b91ec62218dec16b8
Humphrey <vanillaqicecream@gmail.com>
2022-08-07 17:21:09 +0800

    add credits to myself

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,28 +9,40 @@ const Footer = () => {
 
   return (
     <footer className="flex justify-center w-full">
-      <div className="pb-2 flex justify-end items-center gap-3 w-full max-w-screen-xl px-4 font-mono">
-        <a
-          href="https://www.flaticon.com/free-stickers/fast-food"
-          title="fast food stickers"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="text-xs hover:text-gray-500">
-            Assets from flaticon
-          </span>
-        </a>
-        <span>&bull;</span>
-        <a
-          href="https://www.yelp.com/developers/documentation/v3/get_started"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p className="text-xs gap-1 flex items-center justify-center hover:text-gray-500">
-            <span className="px-1">Powered by</span>
-            <Image src={yelpLogo} alt="Yelp Logo" height="20" width="50" />
-          </p>
-        </a>
+      <div className="pb-2 flex justify-between items-center gap-3 w-full max-w-screen-xl px-4 font-mono">
+        <div>
+          <a
+            href="https://github.com/bananamonkey4655"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-500"
+          >
+            Built by Humphrey 🍌
+          </a>
+        </div>
+        <div className="flex justify-between items-center gap-3">
+          <a
+            href="https://www.flaticon.com/free-stickers/fast-food"
+            title="fast food stickers"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="text-xs hover:text-gray-500">
+              Assets from flaticon
+            </span>
+          </a>
+          <span>&bull;</span>
+          <a
+            href="https://www.yelp.com/developers/documentation/v3/get_started"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="text-xs gap-1 flex items-center justify-center hover:text-gray-500">
+              <span className="px-1">Powered by</span>
+              <Image src={yelpLogo} alt="Yelp Logo" height="20" width="50" />
+            </p>
+          </a>
+        </div>
       </div>
     </footer>
   );
